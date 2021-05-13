984d587efe92b4ff9c9f120bcabeee06f8241ca1
Nikos Siatras <nsiatras@gmail.com>
2021-05-13 19:28:05 +0300

    Changes

--- Grbl_Esp32/src/Machine.h
+++ Grbl_Esp32/src/Machine.h
@@ -1,13 +1,4 @@
 #pragma once
 
-// This file is where you choose the machine type, by including
-// one or more machine definition files as described below.
 
-
-// To build for the SourceRabbit 4Axis CNC Motherboard
-// uncomment the following line
-//#include "Machines/sourcerabbit_4axis_cnc_motherboard.h"
-
-// To build for the SourceRabbit Micro Mill 4 Axis
-// uncomment the following line
-#include "Machines/sourcerabbit_MicroMill_4Axis.h"
+#include "Machines/sourcerabbit_ZX7045_4Axis.h"
